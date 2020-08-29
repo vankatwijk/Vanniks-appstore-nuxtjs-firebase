a8e340c9eaf87a09118230ef0f66d2cc5cacd472
Hendrikus van Katwijk <hendrikus.hpvk@gmail.com>
2020-08-29 15:59:28 +0200

    post preview to component

--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,23 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="features-posts">
-
-      <nuxt-link :to="'/posts/'+1" class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image:url('http://maltawinds.com/wp-content/uploads/2019/10/tech-skills-640x360.jpeg')"></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
-
-      <nuxt-link :to="'/posts/'+2" class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image:url('http://maltawinds.com/wp-content/uploads/2019/10/tech-skills-640x360.jpeg')"></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
+    <section class="featured-posts">
+      <PostPreview/>
+      <PostPreview/>
+      <PostPreview/>
 
     </section>
   </div>
 </template>
 
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+  components:{
+    PostPreview
+  }
+}
+</script>
 
 <style scoped>
 .intro {
@@ -70,39 +61,4 @@
   justify-content: center;
 }
 
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
-}
 </style>
