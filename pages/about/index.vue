00a30ba9d5bec43651388e1d3614f7ace0d7b1bd
Hendrikus van Katwijk <hendrikus.hpvk@gmail.com>
2020-08-29 14:32:26 +0200

    basic routes setup for post admin and about

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <p>Dummy textDummy textDummy textDummy textDummy textDummy text</p>
+    <p>Dummy textDummy textDummy textDummy textDummy textDummy text</p>
+    <p>Dummy textDummy textDummy textDummy textDummy textDummy text</p>
+    <p>Dummy textDummy textDummy textDummy textDummy textDummy text</p>
+    <p>Dummy textDummy textDummy textDummy textDummy textDummy text</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page{
+  text-align :center;
+  padding:30;
+}
+</style>
