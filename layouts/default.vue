8f5d7513876163f2316056b727cf9b9371c0c4fe
vankatwijk <hendrikus.hpvk@gmail.com>
2020-09-01 11:59:25 +0200

    logic for navigation bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,32 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav"/>
+    <TheSidenav 
+    :show="displaySidenav"
+    @close="displaySidenav = false"
+    />
+
     <Nuxt />
   </div>
 </template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader'
+import TheSidenav from '@/components/Navigation/TheSidenav'
+
+
+export default {
+  components:{
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidenav:false
+    }
+  }
+}
+</script>
 
 <style>
 html {
