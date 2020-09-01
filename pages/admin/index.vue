d5084f9db6d5c8dd1a7e20d381cc0a537645fd5c
vankatwijk <hendrikus.hpvk@gmail.com>
2020-09-01 12:24:05 +0200

    admin route added

---
+++ pages/admin/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="admin-page">
+        <section class="new-post">
+            <button>Create Post</button>
+        </section>
+        <section class="existing-posts">
+            <h1>Existing Posts</h1>
+            <PostList />
+        </section>
+    </div>
+</template>
+
+<script>
+import PostList from '@/components/Posts/PostList'
+export default {
+  components:{
+    PostList
+  }
+}
+</script>
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+</style>
