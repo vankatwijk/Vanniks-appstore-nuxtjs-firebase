dff5abbaf668885db0ddad1a2c55b12902534543
vankatwijk <hendrikus.hpvk@gmail.com>
2020-09-01 15:18:24 +0200

    editing functionality

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+        <AdminPostForm :post="loadedPost"/>
+
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+    components:{
+        AdminPostForm
+    },
+    data(){
+        return {
+            loadedPost:{
+                author:'hendrikus',
+                title:'my awesome post',
+                content: 'super awesome post created for making milk',
+                thumbnailLink: 'http://maltawinds.com/wp-content/uploads/2019/10/tech-skills-640x360.jpeg'
+            }
+        }
+    }
+}
+</script>
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,55 +1,16 @@
 <template>
     <div class="admin-new-post-page">
         <section class="new-post-form"></section>
-        <form @submit.prevent="onSave">
-            <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
-                    
-            <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-                    
-            <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
-                    
-            <AppControlInput
-                    control-type="textarea"
-                    v-model="editedPost.content">Content</AppControlInput>
-                    
-            <AppButton type="submit">Save</AppButton>
-                    
-            <AppButton
-                    type="button"
-                    style="margin-left: 10px"
-                    btn-style="cancel"
-                    @click="onCancel">Cancel</AppButton>
-        </form>
+        <AdminPostForm />
     </div>
 </template>
 
 <script>
-import AppControlInput from '@/components/UI/AppControlInput'
-import AppButton from '@/components/UI/AppButton'
+import AdminPostForm from '@/components/Admin/AdminPostForm'
 
 export default {
     components:{
-        AppControlInput,
-        AppButton
-    },
-    data(){
-        return {
-            editedPost:{
-                author:'',
-                title:'',
-                thumbnailLink:'',
-                content: ''
-            }
-        }
-    },
-    methods:{
-        onSave(){
-            //save the post
-            console.log(this.editedPost);
-        },
-        onCancel(){
-            this.$router.push('/admin');
-        }
+        AdminPostForm
     }
 }
 </script>
