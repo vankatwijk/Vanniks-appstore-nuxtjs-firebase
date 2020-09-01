dfee3b07a19749d22b39d0ca8640d059f1ac1089
vankatwijk <hendrikus.hpvk@gmail.com>
2020-09-01 15:01:37 +0200

    connecting the create post button

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="admin-page">
         <section class="new-post">
-            <button>Create Post</button>
+            <button @click="$router.push('/admin/new-post')">Create Post</button>
         </section>
         <section class="existing-posts">
             <h1>Existing Posts</h1>
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,8 +1,48 @@
 <template>
     <div class="admin-new-post-page">
         <section class="new-post-form"></section>
-        <form>
-            
+        <form @submit.prevent="onSave">
+            <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+                    
+            <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+                    
+            <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+                    
+            <AppControlInput
+                    control-type="textarea"
+                    v-model="editedPost.content">Content</AppControlInput>
+                    
+            <AppButton type="submit">Save</AppButton>
+                    
+            <AppButton
+                    type="button"
+                    style="margin-left: 10px"
+                    btn-style="cancel"
+                    @click="onCancel">Cancel</AppButton>
         </form>
     </div>
 </template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            editedPost:{
+                author:'',
+                title:'',
+                thumbnailLink:'',
+                content: ''
+            }
+        }
+    },
+    methods:{
+        onSave(){
+            //save the post
+        },
+        onCancel(){
+            
+        }
+    }
+}
+</script>
