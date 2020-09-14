90fcbc4f4bdb764e9b44e7d39263a64b3fcb597c
Hendrikus van Katwijk <hendrikus.hpvk@gmail.com>
2020-09-14 23:43:54 +0200

    testing middleware

---
+++ middleware/log.js
@@ -0,0 +1,5 @@
+function(context){
+
+  console.log('[middleware] the log middleware is running')
+
+}
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,6 +9,7 @@
 
 <script>
 export default {
+  middleware:"log",
   computed:{
     loadedPosts(){
       return this.$store.getters.loadedPosts
