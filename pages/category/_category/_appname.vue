f380f5203026b28d4131625369c71ea9621c5b74
hendrikus.van.katwijk <hendrikus.van.katwijk@internetvikings.com>
2021-04-07 18:19:36 +0200

    go to app button and app description

--- pages/category/_category/_appname.vue
+++ pages/category/_category/_appname.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="single-post-page">
+  <div class="single-app-page">
     <section>
-      <h1 class="post-title"></h1>
-      <div class="post-details" >
-          <div class="post-detail">{{ loadedApp.title}}</div>
-          <div class="post-detail">{{loadedApp.description}}</div>
+
+    <!-- <div class="slider-container">
+        <div class="scrollmenu">
+            <slot name="content"></slot>
+        </div>
+    </div> -->
+
+      <v-slide-group
+        class="pa-4 pr-0 pl-0"
+        active-class="success"
+        show-arrows
+      >
+                              
+          <v-slide-item
+          cols="12" 
+          sm="12" 
+          md="6" 
+          lg="4" 
+          v-for="screenshot in loadedApp.screenshots"
+          :key="screenshot"
+          class="slider-item pa-5">
+
+            <span>
+              <img height="200px" :src='screenshot'>
+            </span>
+
+          </v-slide-item>
+      </v-slide-group>
+
+
+      <h1 class="app-title">{{ loadedApp.title}}</h1>
+      <div class="app-details" >
+          <a :href="loadedApp.url"><div>Go To App</div></a>
+          <div class="app-detail">{{ loadedApp.title}}</div>
+          <div class="app-detail">{{loadedApp.description}}</div>
+          <div class="app-detail">{{loadedApp.keywords}}</div>
+              <iframe height="200px" :src='loadedApp.report'></iframe>
       </div>
     </section>
-    <section class="post-feedback">
+    <section class="app-feedback">
       <p>Do you need an App or custom CRM / CMS build, contact us <a href="mailto:hendrikus@vanniks.com">info@vanniks.com</a></p>
     </section>
   </div>
@@ -31,28 +64,28 @@ export default {
 </script>
 
 <style scoped>
-.single-post-page {
+.single-app-page {
   padding: 30px;
   text-align: center;
   box-sizing: border-box;
 }
 
-.post {
+.app {
   width: 100%;
 }
 
 @media (min-width: 768px) {
-  .post {
+  .app {
     width: 600px;
     margin: auto;
   }
 }
 
-.post-title {
+.app-title {
   margin: 0;
 }
 
-.post-details {
+.app-details {
   padding: 10px;
   box-sizing: border-box;
   border-bottom: 3px solid #ccc;
@@ -63,23 +96,23 @@ export default {
 }
 
 @media (min-width: 768px) {
-  .post-details {
+  .app-details {
     flex-direction: row;
   }
 }
 
-.post-detail {
+.app-detail {
   color: rgb(88, 88, 88);
   margin: 0 10px;
 }
 
-.post-feedback a {
+.app-feedback a {
   color: red;
   text-decoration: none;
 }
 
-.post-feedback a:hover,
-.post-feedback a:active {
+.app-feedback a:hover,
+.app-feedback a:active {
   color: salmon;
 }
 </style>
